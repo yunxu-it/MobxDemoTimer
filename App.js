401d358ae3579c83c7386ec61de55b01eb0b8176
lxlong <yunxu.it@outlook.com>
2018-03-23 11:15:20 +0800

    :sparkles: add af video

--- App.js
+++ App.js
@@ -5,34 +5,17 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import VideoPlayer from 'react-native-af-video-player'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const url = "http://10.10.7.187:8008/2013-2014-2/course/1/1/kejian/video/170331093811717750.mp4"
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <VideoPlayer url={url}/>
       </View>
     );
   }
@@ -56,3 +39,4 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
