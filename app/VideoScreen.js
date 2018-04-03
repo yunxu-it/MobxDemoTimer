dce7b5db896b808b12b74f2a391a2905cd55180b
沉醉不知归路 <yunxu.it@outlook.com>
2018-04-03 10:07:18 +0800

    限制拖动和记录进度

--- app/VideoScreen.js
+++ app/VideoScreen.js
@@ -5,18 +5,19 @@
  */
 
 import React from 'react'
-import { Alert, View, StyleSheet, ScrollView, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import Video from './components/VideoScrollHandler'
 
-// const url = 'http://10.10.7.187:8008/2013-2014-2/course/1/1/kejian/video/170331093811717750.mp4'
-const url = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
+const url = 'http://10.10.7.187:8008/2013-2014-2/course/1/1/kejian/video/170331093811717750.mp4'
+// const url = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
 
 export default class VideoScreen extends React.Component {
   constructor () {
     super()
     this.state = {
       fullScreen: false,
-      maxProgress: 30
+      maxProgress: 0.5,
+      latestProgress: 0.3
     }
   }
 
@@ -29,9 +30,15 @@ export default class VideoScreen extends React.Component {
       <View style={styles.container}>
         <Video url={url}
                maxProgress={this.state.maxProgress}
+               latestProgress={this.state.latestProgress}
                title={'测试视频'}
-               onProgress={(e) => {
-                 console.log('onProgress ' + JSON.stringify(e))
+               onProgress={(time, maxProgress) => {
+                 this.setState(preState => {
+                   return {
+                     maxProgress: maxProgress
+                   }
+                 })
+                 console.log('onProgress ' + JSON.stringify(time))
                }}
                onSeek={(e) => {
                  console.log('onSeek ' + JSON.stringify(e))
