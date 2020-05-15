6114bb3303269916e24d149c3c4242396c202c5e
lxlong <yunxu.it@outlook.com>
2020-05-15 16:37:52 +0800

    :sparkles: 添加注释

--- App.js
+++ App.js
@@ -9,77 +9,63 @@
 import 'react-native-gesture-handler'
 import * as React from 'react'
 
-import { NavigationContainer, useLinking } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { ProfileScreen } from './app/pages/DetailScreen'
 import ChatScreen from './app/pages/ChatScreen'
 import HomeScreen from './app/pages/HomeScreen'
 
-const config = {
-  HomeStack: {
-    path: 'stack',
-    initialRouteName: 'Profile',
-    screens: {
-      Home: 'home',
-      Profile: {
-        path: 'user/:id/:age',
-        parse: {
-          id: id => `is, ${id}`,
-          age: Number
-        }
-        // 接收参数格式 mychat://stack/user/winxo/15?content=这是内容
-      }
-    }
-  },
-  Chat: {
-    path: 'chat',
-    // 可接受参数 mychat://chat?content=这是内容
-  }
-}
-
 const HomeStack = () => {
-  const MyStack = createStackNavigator()
+  const HomeStack = createStackNavigator()
 
   return (
-    <MyStack.Navigator>
-      <MyStack.Screen name="Home" component={HomeScreen}/>
-      <MyStack.Screen name="Profile" component={ProfileScreen}/>
-    </MyStack.Navigator>
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} options={{
+        headerShown: false
+      }}/>
+      <HomeStack.Screen name="Profile" component={ProfileScreen}/>
+    </HomeStack.Navigator>
   )
 }
 
-const Stack = createStackNavigator()
-
 export default function App () {
-  const ref = React.useRef()
+  const Stack = createStackNavigator()
 
-  const { getInitialState } = useLinking(ref, {
+  const config = {
+    HomeStack: {
+      path: 'stack',
+      initialRouteName: 'Profile',
+      screens: {
+        Home: 'home',
+        Profile: {
+          path: 'user/:id/:age',
+          parse: {
+            id: id => `is, ${id}`,
+            age: Number
+          }
+          /*
+            接收参数格式 mychat://stack/user/winxo/15?content=这是内容
+           */
+        }
+      }
+    },
+    Chat: {
+      path: 'chat',
+      // 可接受参数 mychat://chat?content=这是内容
+    }
+  }
+
+  const linking = {
     prefixes: ['mychat://'],
     config
-  })
-
-  const [isReady, setIsReady] = React.useState(false)
-  const [initialState, setInitialState] = React.useState()
-
-  React.useEffect(() => {
-    getInitialState()
-      .catch(() => {})
-      .then(state => {
-        if (state !== undefined) {
-          setInitialState(state)
-        }
-        setIsReady(true)
-      })
-  }, [getInitialState])
-
-  if (!isReady) {
-    return null
   }
 
   return (
-    <NavigationContainer initialState={initialState} ref={ref}>
+    <NavigationContainer linking={linking}>
       <Stack.Navigator>
-        <Stack.Screen name="HomeStack" component={HomeStack}/>
+        <Stack.Screen name="HomeStack" component={HomeStack} options={{
+          headerShown: false
+        }}/>
         <Stack.Screen name="Chat" component={ChatScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
---
+++ README.md
@@ -0,0 +1,29 @@
+## 深层页面跳转
+
+### Android
+
+1. 配置 `AndroidManifest.xml` 意图
+
+```xml
+<activity
+    android:name=".MainActivity"
+    android:launchMode="singleTask">
+    <intent-filter>
+        <action android:name="android.intent.action.MAIN" />
+        <category android:name="android.intent.category.LAUNCHER" />
+    </intent-filter>
+    // 以下为配置部分
+    <intent-filter>
+        <action android:name="android.intent.action.VIEW" />
+        <category android:name="android.intent.category.DEFAULT" />
+        <category android:name="android.intent.category.BROWSABLE" />
+        <data android:scheme="mychat" />
+    </intent-filter>
+</activity>
+```
+
+2. 按照 [App.js](App.js) 文件配置`linking`
+
+### iOS
+
+
